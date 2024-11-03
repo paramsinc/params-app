d233fe91ff9d6a445fad5df7b6a5f89225488849
Fernando Rojo <frojo@sas.upenn.edu>
2024-11-03 10:58:50 -0500

    names

--- apps/next/app/params.json/route.tsx
+++ apps/next/app/params.json/route.tsx
@@ -3,7 +3,7 @@ import { zodToJsonSchema } from 'zod-to-json-schema'
 
 const fileTree = z.record(z.string(), z.union([z.string(), z.record(z.string(), z.string())]))
 
-const mySchema = z
+export const paramsJson = z
   .object({
     docs: z.object({
       main: z.string(),
@@ -17,28 +17,8 @@ const mySchema = z
   })
   .describe('Describe your Params repository.')
 
-const jsonSchema = zodToJsonSchema(mySchema, 'mySchema')
+const jsonSchema = zodToJsonSchema(paramsJson, 'paramsJson')
 
 export function GET() {
   return Response.json(jsonSchema)
-  return Response.json({
-    docs: {
-      main: 'readme.md',
-      sidebar: {
-        Introduction: 'introduction.md',
-        Installation: 'installation.md',
-        Serving: {
-          Introduction: 'serving/introduction.md',
-          Deployment: 'serving/deployment.md',
-          Configuration: 'serving/configuration.md',
-        },
-        Usage: 'usage.md',
-        FAQ: 'faq.md',
-      },
-    },
-    notebook: {
-      mode: 'generate',
-      steps: [{ file: 'notebooks/01-introduction.ipynb' }],
-    },
-  })
 }

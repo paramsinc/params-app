f0aa416d57a1b91c1dba2898c1c02f88c658cb77
Fernando Rojo <frojo@sas.upenn.edu>
2024-11-14 17:09:43 -0500

    updates

--- apps/next/pages/profiles/[profileSlug]/index.tsx
+++ apps/next/pages/profiles/[profileSlug]/index.tsx
@@ -1,3 +1,4 @@
+import { d, db, schema } from 'app/db/db'
 import { ProfileDetailPublicPage } from 'app/features/profile/detail-public/page'
 import { ssgApi } from 'app/trpc/ssg'
 
@@ -8,8 +9,28 @@ export default function ProfileDetailPublicPageWrapper() {
 import type { GetStaticPaths, GetStaticProps } from 'next'
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const profiles = await db
+    .selectDistinctOn([schema.profiles.id], {
+      profileSlug: schema.profiles.slug,
+    })
+    .from(schema.profiles)
+    .where(
+      d.exists(
+        db
+          .select({
+            id: schema.repositories.id,
+          })
+          .from(schema.repositories)
+          .innerJoin(
+            schema.githubRepoIntegrations,
+            d.eq(schema.repositories.id, schema.githubRepoIntegrations.repo_id)
+          )
+      )
+    )
+    .limit(50)
+    .execute()
   return {
-    paths: [],
+    paths: profiles.map((p) => ({ params: { profileSlug: p.profileSlug } })),
     fallback: true,
   }
 }
@@ -20,6 +41,7 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
   await Promise.all([
     ssgApi.profileBySlug_public.prefetch({ profile_slug: profileSlug }),
     ssgApi.onetimePlansByProfileSlug_public.prefetch({ profile_slug: profileSlug }),
+    ssgApi.profileBySlug_public.prefetch({ profile_slug: profileSlug }),
   ])
 
   const trpcState = ssgApi.dehydrate()
